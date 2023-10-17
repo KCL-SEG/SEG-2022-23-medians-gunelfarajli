b9db7e82c4d84d9652d28afb31ccab81eb26a9af
Gunel Farajli <gunel.farajli@kcl.ac.uk>
2023-10-17 01:53:44 +0100

    Find median

--- median.py
+++ median.py
@@ -5,8 +5,15 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        median = None
+        numbers.sort()
+        if len(numbers)%2==0:
+            median = [numbers[len(numbers)//2],numbers[(len(numbers)//2) - 1]]
+        else:
+            median = [numbers[len(numbers)//2]]
+
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(f'The median is: {median}')
